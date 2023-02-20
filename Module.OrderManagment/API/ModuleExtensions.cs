011a16320eea4d32518f359b56097e543f087b2e
MOSAHassan <MOSAHassan@DESKTOP-BTMBS60>
2023-02-20 10:16:47 +0200

    r

--- Module.OrderManagment/API/ModuleExtensions.cs
+++ Module.OrderManagment/API/ModuleExtensions.cs
@@ -6,6 +6,7 @@ using DL.Mapping;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Module.OrderManagment.BL.Abstraction;
+using Module.OrderManagment.BL.Services;
 using Module.OrderManagment.DL.appDBContext;
 using Shared.Infrastructure.Extensions;
 using Shared.Models.Interfaces;
@@ -20,7 +21,8 @@ namespace Module.OrderManagment.API
                 .AddDatabaseContext<OrderManagmentAppDbContext>(configuration)
                 .AddScoped<IOrderManagmentAppDbContext>(provider => provider.GetService<OrderManagmentAppDbContext>());
             services.AddTransient<IUnitOfWork, UnitOfWork>();
-         
+            services.AddTransient<IGetOrderItems, GetOrderItems>();
+
             //var mapperConfig = new MapperConfiguration(mc =>
             //{
             //    mc.AddProfile(new MappingConfigration());
---
+++ Module.OrderManagment/BL/Services/GetOrderItems.cs
@@ -0,0 +1,28 @@
+﻿using BL.Infrastructure;
+using Shared.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.OrderManagment.BL.Services
+{
+    internal class GetOrderItems : IGetOrderItems
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetOrderItems(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public List<object> GetItems(Guid OrderId)
+        {
+            var Items = new List<object>();
+            var items = unitOfWork.OrderItemRepository.GetMany(a=>a.OrderId==OrderId).ToHashSet();
+            foreach (var item in items) { Items.Add(item); }
+            return Items;
+
+        }
+    }
+}
